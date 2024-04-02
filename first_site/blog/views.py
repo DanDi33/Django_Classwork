3e4409d8e1e787d6625ac32da34290499ce4e882
TopAcademy <dan--di@mail.ru>
2024-04-02 21:45:24 +0300

    django 30.03.24

--- first_site/blog/views.py
+++ first_site/blog/views.py
@@ -1,35 +1,37 @@
+from django.contrib import messages
 from django.http import HttpResponse
-from django.shortcuts import render
-
-posts = [
-    {
-        "title": "Test post 1",
-        "author": "John Dou",
-        "content": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been "
-                   "the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of "
-                   "type and scrambled it to make a type specimen book.",
-        "published_at": "October 1 1992"
-    },
-    {
-        "title": "Test post 12",
-        "author": "John Dou",
-        "content": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been "
-                   "the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of "
-                   "type and scrambled it to make a type specimen book.",
-        "published_at": "October 1 1992"
-    }
-]
+from django.shortcuts import render, redirect
+from .models import Post
+from .forms import PostForm
 
 
 # Create your views here.
 
 def home(request):
+    posts = Post.objects.all()
     context = {
         'posts': posts,
-        'title': "Main Blog page"
+        # 'title': "Main Blog page"
 
     }
     return render(request, "blog/home.html", context)
+
+
+def create_post(request):
+    if request.method == "GET":
+        context = {'form': PostForm()}
+        return render(request, 'blog/post_form.html', context)
+    elif request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request,"Success")
+            return redirect('posts')
+        else:
+            messages.error(request, "Error")
+            return render(request, 'blog/post_form.html', {'form': form})
+    else:
+        return False
 
 
 def about(request):
